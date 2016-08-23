3bb2a7fbbe34d3fdef2ebe8bfce207d941e5c48f
djsegal <djsegal@users.noreply.github.com>
2016-08-23 00:56:30 -0400

    Make 'initHighlighting' functions callable

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ module.exports = {
           'default',
           'highlight',
           'highlightAuto',
-          'highlightBlock'
+          'highlightBlock',
+          'initHighlighting',
+          'initHighlightingOnLoad'
         ]
       }
     });
